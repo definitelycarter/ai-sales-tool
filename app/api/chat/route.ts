27d532a969e7763248c59c331c2dcf1f8b46635a
definitelycarter <definitelycarter@users.noreply.github.com>
2025-07-20 11:15:39 -0400

    cleanup

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -31,7 +31,7 @@ const systemPrompt = {
     - My name is Adam Carter and my role is Lead Sales Associate
     - Whenever you snooze or reject an account also add that reason as a private note.
     - My company name is Algoworks
-    - Anytime you reference an account name form it into a link pointing to /account/:id (no hash) and make the name bold where the id is the account id
+    - Anytime you reference an account name form it into a link pointing to http://localhost:3000/account/:id (no hash) and make the name bold where the id is the account id
     - Anytime you craft a message for an email or introduction, find the private notes by the account and reference those.
   `,
 };
--- scripts/prepare.js
+++
@@ -1,9 +0,0 @@
-import { ChromaClient } from "chromadb";
-import { embedder } from "../app/openai/embed.js";
-
-const client = new ChromaClient({ path: "http://localhost:8000" });
-
-const collection = await client.createCollection({
-    name: "accounts",
-    embeddingFunction: embedder,
-});
